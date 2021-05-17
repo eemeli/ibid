c79ab552f922195f9d659aed9839a278e5789f7a
Eemeli Aro <eemeli@gmail.com>
2021-05-17 14:42:14 +0300

    refactor(preset-loader): Simplify

--- src/core/preset-loader.js
+++ src/core/preset-loader.js
@@ -1,59 +1,44 @@
 'use strict'
 
-const nodePath = require('path')
+const { isAbsolute } = require('path')
 
-module.exports = presetLoader(require)
-module.exports.presetLoader = presetLoader
+module.exports = presetLoader
+module.exports.resolvePreset = resolvePreset
 
-function presetLoader(requireMethod) {
-  return path => {
-    let name = ''
-    let scope = ''
-    let absolutePath = ''
+function presetLoader(path) {
+  // Rather than a string preset name, options.preset can be an object
+  // with a "name" key indicating the preset to load; additinoal key/value
+  // pairs are assumed to be configuration for the preset. See the documentation
+  // for a given preset for configuration available.
+  const name = resolvePreset(path)
 
-    if (typeof path === 'string') {
-      name = path.toLowerCase()
-      if (nodePath.isAbsolute(path)) {
-        absolutePath = path
-      }
-    } else if (typeof path === 'object' && path.name) {
-      // Rather than a string preset name, options.preset can be an object
-      // with a "name" key indicating the preset to load; additinoal key/value
-      // pairs are assumed to be configuration for the preset. See the documentation
-      // for a given preset for configuration available.
-      name = path.name.toLowerCase()
-      if (nodePath.isAbsolute(path.name)) {
-        absolutePath = path.name
-      }
-    } else {
-      throw Error('preset must be string or object with key name')
-    }
+  try {
+    const config = require(name)
+    // rather than returning a promise, presets can return a builder function
+    // which accepts a config object (allowing for customization) and returns
+    // a promise.
+    if (typeof config === 'function' && typeof path === 'object')
+      return config(path)
 
-    if (!absolutePath) {
-      if (name[0] === '@') {
-        const parts = name.split('/')
-        scope = parts.shift() + '/'
-        name = parts.join('/')
-      }
-
-      if (!name.startsWith('conventional-changelog-')) {
-        name = `conventional-changelog-${name}`
-      }
-    }
-
-    try {
-      const config = requireMethod(absolutePath || `${scope}${name}`)
-      // rather than returning a promise, presets can return a builder function
-      // which accepts a config object (allowing for customization) and returns
-      // a promise.
-      if (config && !config.then && typeof path === 'object') {
-        return config(path)
-      } else {
-        // require returned a promise that resolves to a config object.
-        return config
-      }
-    } catch (_) {
-      throw Error('does not exist')
-    }
+    // require returned a promise that resolves to a config object.
+    return config
+  } catch (_) {
+    throw Error('does not exist')
   }
+}
+
+function resolvePreset(path) {
+  let name = path && typeof path === 'object' ? path.name : path
+  if (typeof name !== 'string')
+    throw new Error('preset must be string or object with key name')
+  if (isAbsolute(name)) return name
+  let scope = ''
+  if (name[0] === '@') {
+    const parts = name.split('/')
+    scope = parts.shift() + '/'
+    name = parts.join('/')
+  }
+  if (!name.startsWith('conventional-changelog-'))
+    name = `conventional-changelog-${name}`
+  return (scope + name).toLowerCase()
 }
--- src/core/preset-loader.spec.js
+++ src/core/preset-loader.spec.js
@@ -1,126 +1,61 @@
 'use strict'
 
-const chai = require('chai')
-const mocha = require('mocha')
-const describe = mocha.describe
-const it = mocha.it
-const presetLoader = require('./preset-loader').presetLoader
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
-
-chai.use(sinonChai)
-const expect = chai.expect
+const { expect } = require('chai')
+const { describe, it } = require('mocha')
+const { resolvePreset } = require('./preset-loader')
 
 describe('presetLoader', () => {
-  it('loads unscoped package', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
-
-    load('angular')
-
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
-      'conventional-changelog-angular'
-    )
+  it('resolves unscoped package', () => {
+    const res = resolvePreset('angular')
+    expect(res).to.equal('conventional-changelog-angular')
   })
 
-  it('loads unscoped package containing path', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
-
-    load('angular/preset/path')
-
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
-      'conventional-changelog-angular/preset/path'
-    )
+  it('resolves unscoped package containing path', () => {
+    const res = resolvePreset('angular/preset/path')
+    expect(res).to.equal('conventional-changelog-angular/preset/path')
   })
 
-  it('loads unscoped package with full package name', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
-
-    load('conventional-changelog-angular')
-
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
-      'conventional-changelog-angular'
-    )
+  it('resolves unscoped package with full package name', () => {
+    const res = resolvePreset('conventional-changelog-angular')
+    expect(res).to.equal('conventional-changelog-angular')
   })
 
-  it('loads unscoped package with full package name containing path', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
-
-    load('conventional-changelog-angular/preset/path')
-
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
-      'conventional-changelog-angular/preset/path'
-    )
+  it('resolves unscoped package with full package name containing path', () => {
+    const res = resolvePreset('conventional-changelog-angular/preset/path')
+    expect(res).to.equal('conventional-changelog-angular/preset/path')
   })
 
-  it('loads scoped package', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
-
-    load('@scope/angular')
-
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
-      '@scope/conventional-changelog-angular'
-    )
+  it('resolves scoped package', () => {
+    const res = resolvePreset('@scope/angular')
+    expect(res).to.equal('@scope/conventional-changelog-angular')
   })
 
-  it('loads scoped package containing path', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
+  it('resolves scoped package containing path', () => {
+    const res = resolvePreset('@scope/angular/preset/path')
+    expect(res).to.equal('@scope/conventional-changelog-angular/preset/path')
+  })
 
-    load('@scope/angular/preset/path')
+  it('resolves scoped package with full package name', () => {
+    const res = resolvePreset('@scope/conventional-changelog-angular')
+    expect(res).to.equal('@scope/conventional-changelog-angular')
+  })
 
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
+  it('resolves scoped package with full package name containing path', () => {
+    const res = resolvePreset(
       '@scope/conventional-changelog-angular/preset/path'
     )
+    expect(res).to.equal('@scope/conventional-changelog-angular/preset/path')
   })
 
-  it('loads scoped package with full package name', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
-
-    load('@scope/conventional-changelog-angular')
-
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
-      '@scope/conventional-changelog-angular'
-    )
-  })
-
-  it('loads scoped package with full package name containing path', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
-
-    load('@scope/conventional-changelog-angular/preset/path')
-
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
-      '@scope/conventional-changelog-angular/preset/path'
-    )
-  })
-
-  it('loads package with an absolute file path', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
+  it('resolves package with an absolute file path', () => {
     const filePath = require.resolve('conventional-changelog-angular')
-
-    load(filePath)
-
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
-      filePath
-    )
+    const res = resolvePreset(filePath)
+    expect(res).to.equal(filePath)
   })
 
-  it('loads package with an absolute file path name', () => {
-    const requireMethod = sinon.spy()
-    const load = presetLoader(requireMethod)
+  it('resolves package with an absolute file path name', () => {
     const filePath = require.resolve('conventional-changelog-angular')
-
-    load({ name: filePath })
-
-    expect(requireMethod).to.have.been.calledOnce.and.to.have.been.calledWith(
-      filePath
-    )
+    const res = resolvePreset({ name: filePath })
+    expect(res).to.equal(filePath)
   })
 })
