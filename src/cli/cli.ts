827d218d6cce536091c919d8f5d174ad6d46bfb9
Eemeli Aro <eemeli@gmail.com>
2021-07-14 14:56:40 +0300

    feat(cli): Start on actual implementation, using inquirer for interaction

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -4,6 +4,7 @@ import glob from 'tiny-glob'
 import yargsParser from 'yargs-parser'
 
 import { getCurrentUpdate, PackageUpdate } from '../index'
+import { filterUpdates } from './filter'
 
 // 'fs/promises' is only available from Node.js 14.0.0
 const { readFile } = promises
@@ -28,24 +29,28 @@ async function findPackageRoots(patterns: string[]) {
 
 ;(async function main() {
   const argv = yargsParser(process.argv.slice(2), {
-    alias: { preset: ['p'] },
-    narg: { preset: 1 },
-    string: ['preset']
+    alias: { 'all-commits': ['a'], prerelease: ['p'] },
+    boolean: ['all-commits']
   })
   const updates: PackageUpdate[] = []
   for (const root of await findPackageRoots(argv._))
-    updates.push(await getCurrentUpdate(root))
-  if (updates.length === 0)
-    throw new Error(`No packages found in: ${argv._.join(', ')}`)
-  console.dir(
-    updates.map(up => ({
-      cwd: up.context.cwd,
-      name: up.context.package?.name,
-      prev: up.context.package?.version,
-      bump: up.bump,
-      next: up.version,
-      cl: up.commits.length
-    })),
-    { depth: null }
-  )
+    updates.push(
+      await getCurrentUpdate(root, { bumpAllChanges: !!argv.allCommits })
+    )
+  if (updates.length === 0) {
+    console.error(`No packages found in: ${argv._.join(', ')}`)
+    process.exit(1)
+  }
+
+  const apply = await filterUpdates(updates)
+  if (!apply) {
+    console.error('Not applying any updates.')
+    return
+  }
+
+  const bumps = updates.filter(up => up.bump)
+  if (bumps.length === 0) {
+    console.error(`No packages to update.`)
+    return
+  }
 })()
---
+++ src/cli/filter.ts
@@ -0,0 +1,161 @@
+import { prompt } from 'inquirer'
+import { applyBump, ReleaseType } from '../bump/bump'
+
+import type { PackageUpdate } from '../index'
+
+const isBump = (bump: string): bump is ReleaseType =>
+  [
+    'major',
+    'minor',
+    'patch',
+    'premajor',
+    'preminor',
+    'prepatch',
+    'prerelease'
+  ].includes(bump)
+
+const plural = (count: number, name: string) =>
+  count === 1 ? `1 ${name}` : `${count} ${name}s`
+
+export function updateStrings(updates: PackageUpdate[]): {
+  print: string
+  edit: string
+} {
+  let edit = ''
+  const printChanged: string[] = []
+  const unchanged: string[] = []
+  for (const { context, commits, bump, version: next } of updates) {
+    const { name, version } = context.package || {}
+    if (bump) {
+      const cs = plural(commits.length, 'commit')
+      printChanged.push(`${name}: ${bump} (${version} -> ${next}, ${cs})`)
+
+      let cmd: string = bump
+      if (bump === 'set') cmd = `set=${next}`
+      else {
+        const { prerelease } = context.config
+        if (typeof prerelease === 'string') cmd = `${bump}=${prerelease}`
+      }
+      edit += `${cmd} ${name} (${version}, ${cs})\n`
+    } else {
+      let vs = version
+      if (commits.length > 0)
+        vs += `, ${plural(commits.length, 'commit')}`
+      unchanged.push(`keep ${name} (${vs})`)
+    }
+  }
+
+  const print =
+    `Updating ${printChanged.length}/${plural(updates.length, 'package')}` +
+    (printChanged.length === 0
+      ? '.'
+      : ':\n\n    ' + printChanged.join('\n    ')) +
+    '\n'
+
+  if (unchanged.length > 0) {
+    edit += '\n'
+    for (const u of unchanged) edit += `${u}\n`
+  }
+
+  edit += `
+# Update package versions
+#
+# Commands:
+# keep <name> = do not update this package
+# patch <name> = patch release, e.g. 1.2.3 -> 1.2.4 or 0.1.2-3 -> 0.1.2
+# minor <name> = minor release, e.g. 1.2.3 -> 1.3.0 or 0.1.2-3 -> 0.2.0
+# major <name> = major release, e.g. 1.2.3 -> 2.0.0 or 0.1.2-3 -> 1.0.0
+# prerelease <name> = prerelease, e.g. 1.2.3 -> 1.2.3-0 or 1.2.3-foo.4 -> 1.2.3-foo.5
+# preminor <name> = prerelease with minor update, e.g. 1.2.3 -> 1.3.0-0 or 1.2.3-foo.4 -> 1.3.0-foo.0
+# premajor <name> = prerelease with major update, e.g. 1.2.3 -> 2.0.0-0 or 1.2.3-foo.4 -> 2.0.0-foo.0
+# set=<version> <name> = set a specific version
+#
+# Each of the pre* commands may use a string argument to set an identifier:
+#     preminor=bar <name>    # would update 1.2.3-foo.4 -> 1.3.0-bar.0
+# Identifiers must contain only . - and word characters. An empty string is a valid identifier.
+`
+
+  return { print, edit }
+}
+
+export async function filterUpdates(
+  updates: PackageUpdate[]
+): Promise<boolean> {
+  const { print, edit } = updateStrings(updates)
+  console.log(print)
+  const answers = await prompt<{
+    action: 'Edit updates' | 'Yes' | 'No'
+    edit?: string
+  }>([
+    {
+      name: 'action',
+      message: 'Apply these updates?',
+      type: 'list',
+      choices: ['Edit updates', 'Yes', 'No'],
+      default: 'Yes'
+    },
+    {
+      name: 'edit',
+      type: 'editor',
+      when: answers => answers.action === 'Edit updates',
+      message: 'Edit list of updates',
+      default: edit
+    }
+  ])
+
+  if (answers.action === 'Yes') return true
+  if (answers.action === 'No') return false
+
+  const editSrc = answers.edit?.trim() || ''
+  const commands = new Map<
+    string,
+    { bump: ReleaseType | 'set'; id?: string; line: string }
+  >()
+  for (const line of editSrc.split(/\s*\n\s*/)) {
+    if (/^(#|keep\b)/.test(line)) continue
+    const [command, name] = line.split(/\s+/)
+    const [bump, id] = command.split('=')
+    if (bump === 'set' || isBump(bump)) commands.set(name, { bump, id, line })
+    else console.error(`Invalid bump ${bump}: ${line}`)
+  }
+
+  for (const up of updates) {
+    if (!up.context.package) continue
+    const { name } = up.context.package
+    const cmd = commands.get(name)
+    if (cmd) {
+      const { bump, id, line } = cmd
+      commands.delete(name)
+
+      if (bump === 'set') {
+        if (!id || /[^\w.-]/.test(id))
+          console.error(`Ignoring invalid version ${id}: ${line}`)
+        else {
+          up.bump = bump
+          up.version = id
+        }
+      } else {
+        const { config } = up.context
+        if (typeof id === 'string') {
+          if (/[^\w.-]/.test(id))
+            console.error(`Ignoring invalid identifier ${id}: ${line}`)
+          if (bump.startsWith('pre')) config.prerelease = id
+          else
+            console.error(
+              `Ignoring identifier ${id} for ${bump} release: ${line}`
+            )
+        } else if (typeof config.prerelease === 'string') {
+          config.prerelease = bump.startsWith('pre')
+        }
+        up.bump = bump
+        up.version = applyBump(up.context, bump)
+      }
+    } else {
+      up.bump = null
+      up.version = null
+    }
+  }
+
+  console.log('')
+  return filterUpdates(updates)
+}
