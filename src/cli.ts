cc917be7c45439de6f7e71a6763a4917873f9df0
Eemeli Aro <eemeli@gmail.com>
2021-07-25 23:34:41 +0300

    feat(cli): Add a unified CLI frontend

---
+++ src/cli.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import { InputError } from './cli-helpers/input-error'
+import { depend } from './depend/depend'
+import { version } from './version/version'
+
+let command
+switch (process.argv[2]) {
+  case 'depend':
+    command = depend
+    break
+  case 'version':
+    command = version
+    break
+  default:
+    console.error(`Usage: ibid depend|version [options]`)
+    process.exit(1)
+}
+
+const args = process.argv.slice(3)
+command(args, process.stderr).catch(error => {
+  if (error instanceof InputError) {
+    console.error(error.message)
+    process.exit(1)
+  } else {
+    console.error(error)
+    process.exit(2)
+  }
+})
